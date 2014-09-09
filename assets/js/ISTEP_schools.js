20a8953064ea54c6624851124016d8155ef8a205
Peter Jacobs <peter.jacobs@ambassador-enterprises.com>
2014-09-09 13:47:00 -0400

    Use maps.indiana.edu for authoritative school coordinates

--- assets/js/ISTEP_schools.js
+++ assets/js/ISTEP_schools.js
@@ -31,23 +31,60 @@ var map;
 	}
 	function loadSchools(){
 		$.getJSON("//indianalearns.info/api/v1/schools/",function(data){	
-			var count = 1;
+			var count = 0;
 			$.each(data,function(index,d){
 				count = count + 1;
-				if(count>5){return false;}
-				var add = encodeURIComponent(d.address);
-				var zip = encodeURIComponent(d.zip);
-				$.getJSON("https://maps.googleapis.com/maps/api/geocode/json?components=street_address:" + add +"|postal_code:"+zip,function(data){
-					var lat = data.results[0].geometry.location.lat;
-					var lng = data.results[0].geometry.location.lng;
-					var pos = new google.maps.LatLng(lat,lng);
+				if(count>10){return false;}
 
+				projectGISToLatLon(d.gis_url, function(pos) {
 					var mark = new google.maps.Marker({
-						postion: pos,
 						map: map,
+						position: pos,
+						title: d.name,
 						animation: google.maps.Animation.DROP
 					});
 				});
+				// var add = encodeURIComponent(d.address);
+				// var zip = encodeURIComponent(d.zip);
+				// $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?components=street_address:" + add +"|postal_code:"+zip,function(data){
+				// 	var lat = data.results[0].geometry.location.lat;
+				// 	var lng = data.results[0].geometry.location.lng;
+				// 	var pos = new google.maps.LatLng(lat,lng);
+
+				// 	var mark = new google.maps.Marker({
+				// 		postion: pos,
+				// 		map: map,
+				// 		animation: google.maps.Animation.DROP
+				// 	});
+				// });
+			});
+		});
+	}
+	function projectGISToLatLon(url, callback) {
+		// take an ArcGIS url and project its geometry into lat/lon
+		$.getJSON(url, function(data) {
+			var spatialRefID = data.spatialReference.wkid;
+			var outputSpatialRefID = 4326; // constant, refers to lat/lon coordinate system
+			
+			var geometries = {
+				geometryType: data.geometryType,
+				geometries: [data.features[0].geometry]
+			};
+			
+			var projectQueryString = "project" 
+				    + "?inSR="+spatialRefID
+				    + "&outSR="+outputSpatialRefID
+				    + "&geometries="+encodeURIComponent(JSON.stringify(geometries))
+				    + "&f=json";
+			
+			var serviceURL = "http://maps.indiana.edu/arcgis/rest/services/Utilities/Geometry/GeometryServer/";
+			serviceURL += projectQueryString;
+			
+			$.getJSON(serviceURL, function(result) {
+				if(callback && typeof callback === "function") {
+					callback(new google.maps.LatLng(result.geometries[0].y,
+					                                result.geometries[0].x));
+				}
 			});
 		});
 	}
