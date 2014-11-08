a352329559d0b3cf506817ebe99bb6e2817c639c
Peter Jacobs <auchwertin@gmail.com>
2014-11-08 13:05:52 -0500

    Collect graduation rates & student/teacher ratios

--- assets/js/hack.js
+++ assets/js/hack.js
@@ -1,1 +1,16 @@
-console.log("start");
+(function(){
+    // corporation graduation rates, pulled from indianalearns api
+    var graduation_rates_url = '/api/v1/reports/corporation/graduation_rates?group=total';
+    var student_teacher_ratio_url = '/api/v1/reports/corporation/student_teacher_ratios';
+
+    $.getJSON(student_teacher_ratio_url, function(student_teacher_ratios) {
+        console.log("student_teacher_ratios:");
+        console.log(student_teacher_ratios);
+    });
+
+    $.getJSON(graduation_rates_url, function(graduation_rates) {
+        console.log("graduation_rates:");
+        console.log(graduation_rates);
+    });
+
+})();
